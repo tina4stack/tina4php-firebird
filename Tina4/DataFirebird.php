e68918c66b3a3e10adf25f5320175c645fc2dc21
Andre van Zuydam <andrevanzuydam@gmail.com>
2022-05-25 00:36:00 +0200

    Add short name for migrations Updated packages

--- Tina4/DataFirebird.php
+++ Tina4/DataFirebird.php
@@ -225,4 +225,13 @@ class DataFirebird implements DataBase
     {
         return false;
     }
+
+    /**
+     * Get a short name for the database used for specific database migrations
+     * @return string
+     */
+    public function getShortName(): string
+    {
+        return "firebird";
+    }
 }
